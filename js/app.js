bc9a69c0abe8a5ae506e05c5bbf1989cdae3b0f4
Kazi Monirul Islam <jobatanycost@gmail.com>
2022-09-24 13:06:54 +0600

    01. remove all the id from html

--- js/app.js
+++ js/app.js
@@ -4,18 +4,20 @@ const readMenu = document.querySelector(".read-menu");
 
 readMenu.addEventListener("click",function(){
     // selecting all the notification by ID and add a class for remove background
-    let notification = document.querySelectorAll("#not");
+    let notification = document.querySelectorAll(".not01,.not02,.not03");
 
     for(let j=0;j<notification.length;j++){
         notification[j].classList.add("active");
-    }
-    
+    } 
     //slecting all the fontawesome icon and remove from the html  
-    let circleIcon = document.querySelectorAll("#icon");
+    let circleIcon = document.querySelectorAll("i");
     console.log(circleIcon);
     for(let i=0;i<circleIcon.length;i++){
         circleIcon[i].remove();
     }
+    //message count zero
+    let messageCount = document.getElementById("notification_count");
+    messageCount.textContent = 0;
 
 })
     // message read when clicked
